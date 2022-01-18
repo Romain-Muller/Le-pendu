4783dcfbae346a69063057afb17848ef8db25a2f
Romain Muller <romain.muller.pro@outlook.fr>
2022-01-18 22:18:23 +0100

    Retrait des guillemets dans la liste de mots

--- defaut.js
+++ defaut.js
@@ -1,17 +1,23 @@
 const url = 'https://raw.githubusercontent.com/Romain-Muller/Le-pendu/main/liste_francais.txt';
-async function recupererPrix () {
+
+async function recupererMots () { //Récupérer les mots provenant d'une liste au format .txt hébergée sut github.
     const requete = await fetch(url, {
         method: 'GET',
     });
 
     if(requete.ok) {
-        let donnees = await requete.text();
-        console.log(donnees)
-        let listeDeMots = donnees.split('\t');
-        console.log(listeDeMots);
+        let mots = await requete.text(); 
+        console.log(mots)
+        let listeDeMots = mots.split('\n'); //Créer un tableaux contenant chaque mots. On utilise \n car on prend chaque chaines de charactères séparés d'un retour à la ligne.
+        document.body.innerHTML = '<p>' + listeDeMots[2] + '</p>';
+        listeDeMots.forEach((element) => { 
+            if(element.match('�')) { //Ici, on vérifie si un mot contient le charactère �. Le but étant de ne pas afficher le mot si il contient ce charactère.
+                console.log(element.match('�'));
+            }
+        });
     } else {
         alert('Un problème est survenu.');
     }
 } 
 
-recupererPrix();
+recupererMots();
